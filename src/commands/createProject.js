958a0967023443941fb717ddf49c59cfb669b550
RNT13 <renatornt13@gmail.com>
2025-08-18 22:31:42 -0300

    Fix: 5.3.5 - Correção da tipagem de colorUtils

--- src/commands/createProject.js
+++ src/commands/createProject.js
@@ -261,7 +261,7 @@ import { ColorVariants } from '@/utils/colorUtils'
 import 'styled-components'
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends DefaultTheme {
+  export interface DefaultTheme {
     colors: {
       baseBlue: ColorVariants
       baseGreen: ColorVariants
@@ -408,7 +408,27 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
     path.join(appPath, "src/utils/colorUtils.ts"),
     `// 🎨 COLOR UTILS - Utilitários para geração de variantes de cores HSL
 
-export function colorHSLVariants(h: number, s: number, l: number) {
+export type ColorVariants = {
+  base: string
+  light: string
+  light02: string
+  light04: string
+  light08: string
+  light20: string
+  light30: string
+  light40: string
+  light50: string
+  dark: string
+  dark02: string
+  dark04: string
+  dark08: string
+  dark20: string
+  dark30: string
+  dark40: string
+  dark50: string
+
+
+export function colorHSLVariants(h: number, s: number, l: number): ColorVariants {
   const clamp = (val: number) => Math.min(100, Math.max(0, val))
   return {
     base: \`hsl(\${h}, \${s}%, \${clamp(l)}%)\`,
